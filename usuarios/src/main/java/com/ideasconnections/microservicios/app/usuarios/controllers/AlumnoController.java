c6241af2e1ef7a3cc726e3e93c66b36efd55fda1
mramoss <moi6ramoss@gmail.com>
2020-05-14 17:33:50 -0400

    Crear controller implementacion eliminar,editar,crear

--- usuarios/src/main/java/com/ideasconnections/microservicios/app/usuarios/controllers/AlumnoController.java
+++ usuarios/src/main/java/com/ideasconnections/microservicios/app/usuarios/controllers/AlumnoController.java
@@ -4,10 +4,9 @@ package com.ideasconnections.microservicios.app.usuarios.controllers;
 import com.ideasconnections.microservicios.app.usuarios.models.entity.Alumno;
 import com.ideasconnections.microservicios.app.usuarios.services.AlumnoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -18,18 +17,48 @@ public class AlumnoController {
     private AlumnoService service;
 
     @GetMapping
-    public ResponseEntity<?> listar(){
+    public ResponseEntity<?> listar() {
+        //htpp status 200
         return ResponseEntity.ok().body(service.findAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<?> ver(@PathVariable Long id){
+    public ResponseEntity<?> ver(@PathVariable Long id) {
         Optional<Alumno> optionalAlumno = service.findById(id);
-        if(!optionalAlumno.isPresent()){
-            ResponseEntity.notFound().build();
+        if (!optionalAlumno.isPresent()) {
+            return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(optionalAlumno.get());
     }
 
+    @PostMapping
+    public ResponseEntity<?> crear(@RequestBody Alumno alumno) {
+        Alumno alumnoDb = service.save(alumno);
+        //htpp status 201
+        return ResponseEntity.status(HttpStatus.CREATED).body(alumnoDb);
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<?> editar(@RequestBody Alumno alumno, @PathVariable Long id) {
+        Optional<Alumno> optionalAlumno = service.findById(id);
+
+        if (!optionalAlumno.isPresent()) {
+            //htpp status 404
+            return ResponseEntity.notFound().build();
+        }
+
+        Alumno alumnoDb = optionalAlumno.get();
+        alumnoDb.setNombre(alumno.getNombre());
+        alumnoDb.setApellido(alumno.getApellido());
+        alumnoDb.setEmail(alumno.getEmail());
+        //htpp status 201
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(alumnoDb));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+        service.deleteById(id);
+        //htpp status 204
+        return ResponseEntity.noContent().build();
+    }
 }
