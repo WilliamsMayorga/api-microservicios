59374f4ca509e5c504db0a9a6b74da6cf186820b
mramoss <moi6ramoss@gmail.com>
2020-05-16 12:27:53 -0400

    Creando microservicio examenes, repository y entity C-32

---
+++ microservicios-examenes/src/main/java/com/ideasconnections/microservicios/app/examenes/models/respository/ExamenRepository.java
@@ -0,0 +1,9 @@
+package com.ideasconnections.microservicios.app.examenes.models.respository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ideasconnections.microservicios.app.examenes.models.entity.Examen;
+
+public interface ExamenRepository extends CrudRepository<Examen, Long> {
+
+}
