cd913ab031fab9bc206690038bf7985c7a9adadb
mramoss <moi6ramoss@gmail.com>
2020-05-16 11:38:03 -0400

    Buscar Alumnos usando operador like

--- microservicios-usuarios/src/main/java/com/ideasconnections/microservicios/app/usuarios/services/AlumnoService.java
+++ microservicios-usuarios/src/main/java/com/ideasconnections/microservicios/app/usuarios/services/AlumnoService.java
@@ -1,7 +1,10 @@
 package com.ideasconnections.microservicios.app.usuarios.services;
 
+import java.util.List;
+
 import com.ideasconnections.microservicios.commons.alumnos.models.entity.Alumno;
 import com.ideasconnections.microservicios.commons.services.CommonService;
 
 public interface AlumnoService extends CommonService<Alumno> {
+    public List<Alumno> findByNombreOrApellido(String term);
 }
