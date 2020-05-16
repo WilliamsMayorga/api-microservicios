3a15958e62a7125ac90a21d95284b34370ba1587
mramoss <moi6ramoss@gmail.com>
2020-05-16 13:04:03 -0400

    Anadiendo relacion bidireccional en examen con sus preguntas C-33

--- microservicios-examenes/src/main/java/com/ideasconnections/microservicios/app/examenes/models/entity/Pregunta.java
+++ microservicios-examenes/src/main/java/com/ideasconnections/microservicios/app/examenes/models/entity/Pregunta.java
@@ -1,10 +1,17 @@
 package com.ideasconnections.microservicios.app.examenes.models.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "preguntas")
@@ -14,6 +21,11 @@ public class Pregunta {
 	private Long id;
 	
 	private String texto;
+	
+	@JsonIgnoreProperties(value = {"preguntas"})
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "examen_id")
+	private Examen examen;
 
 	public Long getId() {
 		return id;
@@ -30,6 +42,29 @@ public class Pregunta {
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
+	public Examen getExamen() {
+		return examen;
+	}
+
+	public void setExamen(Examen examen) {
+		this.examen = examen;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		if(!(obj instanceof Pregunta)) {
+			return false;
+		}
+		Pregunta pregunta = (Pregunta) obj;
+		
+		return this.id !=null && this.id.equals(pregunta.getId());
+	}
+	
+	
 	
 	
 
