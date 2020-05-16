8ad2a963d9877ca3ab0773bd40e318f2b626100a
mramoss <moi6ramoss@gmail.com>
2020-05-16 15:55:06 -0400

    Mejorando editar con stream,filte y forEach

--- microservicios-examenes/src/main/java/com/ideasconnections/microservicios/app/examenes/controllers/ExamenController.java
+++ microservicios-examenes/src/main/java/com/ideasconnections/microservicios/app/examenes/controllers/ExamenController.java
@@ -1,7 +1,4 @@
 package com.ideasconnections.microservicios.app.examenes.controllers;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
@@ -12,36 +9,26 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ideasconnections.microservicios.app.examenes.models.entity.Examen;
-import com.ideasconnections.microservicios.app.examenes.models.entity.Pregunta;
 import com.ideasconnections.microservicios.app.examenes.services.ExamenService;
 import com.ideasconnections.microservicios.commons.controllers.CommonController;
 
 @RestController
 public class ExamenController extends CommonController<Examen, ExamenService> {
-	
+
 	@PutMapping("/{id}")
 	public ResponseEntity<?> editar(@RequestBody Examen examen, @RequestParam Long id) {
 		Optional<Examen> examenOptional = service.findById(id);
-		if(!examenOptional.isPresent()) {
+		if (!examenOptional.isPresent()) {
 			return ResponseEntity.notFound().build();
 		}
 		Examen examenDb = examenOptional.get();
 		examenDb.setNombre(examen.getNombre());
-		
-		List<Pregunta> eliminadas = new ArrayList<>();
-		
-		examenDb.getPreguntas().forEach(preguntaDb -> {
-			if(!examen.getPreguntas().contains(preguntaDb)) {
-				eliminadas.add(preguntaDb);
-			}
-		});
-		
-		eliminadas.forEach(pregunta -> {
-			examenDb.removePregunta(pregunta);
-		});
-		
+
+		examenDb.getPreguntas().stream().filter(preguntaDb -> !examen.getPreguntas().contains(preguntaDb))
+				.forEach(examenDb::removePregunta);
+
 		examenDb.setPreguntas(examen.getPreguntas());
-		
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(service.save(examenDb));
 	}
 }
