29b0c21bf53396495fd3ac2787bbcf32b6a4ec54
mramoss <moi6ramoss@gmail.com>
2020-05-15 20:44:22 -0400

    Asignar y eliminar alumnos al curso C-28

--- microservicios-cursos/src/main/java/com/ideasconnections/microservicios/api/cursos/controllers/CursoController.java
+++ microservicios-cursos/src/main/java/com/ideasconnections/microservicios/api/cursos/controllers/CursoController.java
@@ -1,9 +1,11 @@
 package com.ideasconnections.microservicios.api.cursos.controllers;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.ideasconnections.microservicios.api.cursos.models.entity.Curso;
 import com.ideasconnections.microservicios.api.cursos.services.CursoService;
+import com.ideasconnections.microservicios.commons.alumnos.models.entity.Alumno;
 import com.ideasconnections.microservicios.commons.controllers.CommonController;
 
 @RestController
@@ -26,5 +29,30 @@ public class CursoController extends CommonController<Curso, CursoService> {
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(cursoDb));
 	}
 	
+	@PutMapping("/{id}/asignar-alumnos")
+	public ResponseEntity<?> asignarAlumnos(@RequestBody List<Alumno> alumnos,@PathVariable Long id){
+		Optional<Curso> optionalCurso = this.service.findById(id);
+		if(!optionalCurso.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		Curso cursoDb = optionalCurso.get();
+		alumnos.forEach(alumno -> {
+			cursoDb.addAlumno(alumno);
+		});
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(cursoDb));
+		
+	}
+	
+	@DeleteMapping("/{id}/eliminar-alumnos")
+	public ResponseEntity<?> eliminarAlumno(@RequestBody Alumno alumno,@PathVariable Long id){
+		Optional<Curso> optionalCurso = this.service.findById(id);
+		if(!optionalCurso.isPresent()) {
+			return ResponseEntity.notFound().build();
+		}
+		Curso cursoDb = optionalCurso.get();
+		cursoDb.removeAlumno(alumno);
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.service.save(cursoDb));
+		
+	}
 
 }
