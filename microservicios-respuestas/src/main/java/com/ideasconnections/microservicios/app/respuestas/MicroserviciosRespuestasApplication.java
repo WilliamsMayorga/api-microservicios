4418ae9d4ae465522de4daa56ec2872f5a1809bf
mramoss <moi6ramoss@gmail.com>
2020-05-19 18:31:14 -0400

    Configurando conexión mongo en microservicio respuestas

--- microservicios-respuestas/src/main/java/com/ideasconnections/microservicios/app/respuestas/MicroserviciosRespuestasApplication.java
+++ microservicios-respuestas/src/main/java/com/ideasconnections/microservicios/app/respuestas/MicroserviciosRespuestasApplication.java
@@ -2,13 +2,10 @@ package com.ideasconnections.microservicios.app.respuestas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
-@EntityScan({ "com.ideasconnections.microservicios.app.respuestas.models.entity",
-		"com.ideasconnections.microservicios.commons.examenes.models.entity" })
 public class MicroserviciosRespuestasApplication {
 
 	public static void main(String[] args) {
