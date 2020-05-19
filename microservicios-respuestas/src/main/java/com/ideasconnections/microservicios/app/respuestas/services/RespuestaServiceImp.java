08f1fc69e712786987ffb36cd7bad3c1495437cf
mramoss <moi6ramoss@gmail.com>
2020-05-19 19:37:35 -0400

    CLiente HTTP para comunicar con examenes en microservicio respuestas

--- microservicios-respuestas/src/main/java/com/ideasconnections/microservicios/app/respuestas/services/RespuestaServiceImp.java
+++ microservicios-respuestas/src/main/java/com/ideasconnections/microservicios/app/respuestas/services/RespuestaServiceImp.java
@@ -3,12 +3,18 @@ package com.ideasconnections.microservicios.app.respuestas.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.ideasconnections.microservicios.app.respuestas.clients.ExamenFeignClient;
 import com.ideasconnections.microservicios.app.respuestas.models.entity.Respuesta;
 import com.ideasconnections.microservicios.app.respuestas.models.repository.RespuestaRepository;
+import com.ideasconnections.microservicios.commons.examenes.models.entity.Examen;
 
 @Service
 public class RespuestaServiceImp implements RespuestaService {
+
+	@Autowired
+	private ExamenFeignClient examenClient;
 
 	@Autowired
 	private RespuestaRepository repository;
