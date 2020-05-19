c4ec4ca59bc045438523ec1db7df82f36c12e97f
mramoss <moi6ramoss@gmail.com>
2020-05-19 15:52:48 -0400

    Añadiendo cliente HTTP para comunicación con cursos en microservicios alumnos

--- microservicios-usuarios/src/main/java/com/ideasconnections/microservicios/app/usuarios/services/AlumnoServiceImpl.java
+++ microservicios-usuarios/src/main/java/com/ideasconnections/microservicios/app/usuarios/services/AlumnoServiceImpl.java
@@ -2,16 +2,21 @@ package com.ideasconnections.microservicios.app.usuarios.services;
 
 import java.util.List;
 
+import com.ideasconnections.microservicios.app.usuarios.client.CursoFeignClient;
 import com.ideasconnections.microservicios.app.usuarios.models.repository.AlumnoRepository;
 import com.ideasconnections.microservicios.commons.alumnos.models.entity.Alumno;
 import com.ideasconnections.microservicios.commons.services.CommonServiceImpl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AlumnoServiceImpl extends CommonServiceImpl<Alumno,AlumnoRepository> implements AlumnoService {
 
+	@Autowired
+	private CursoFeignClient clienteCurso;
+	
     @Override
     @Transactional(readOnly = true)
     public List<Alumno> findByNombreOrApellido(String term) {
@@ -22,6 +27,11 @@ public class AlumnoServiceImpl extends CommonServiceImpl<Alumno,AlumnoRepository
 	@Transactional(readOnly = true)
 	public Iterable<Alumno> findAllById(Iterable<Long> ids) {
 		return repository.findAllById(ids);
+	}
+
+	@Override
+	public void eliminarCursoAlumnoPorId(Long id) {
+		clienteCurso.eliminarCursoAlumnoPorId(id);
 	}
 
 }
