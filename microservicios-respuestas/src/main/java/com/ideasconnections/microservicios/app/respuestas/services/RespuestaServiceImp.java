842111261ad4186ec85af6e0af60f8761dbb3cf1
mramoss <moi6ramoss@gmail.com>
2020-05-19 20:58:18 -0400

    Obteniendo preguntas respondidas por alumno y por preguntas Ids distribuidamente

--- microservicios-respuestas/src/main/java/com/ideasconnections/microservicios/app/respuestas/services/RespuestaServiceImp.java
+++ microservicios-respuestas/src/main/java/com/ideasconnections/microservicios/app/respuestas/services/RespuestaServiceImp.java
@@ -1,14 +1,17 @@
 package com.ideasconnections.microservicios.app.respuestas.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import com.ideasconnections.microservicios.app.respuestas.clients.ExamenFeignClient;
 import com.ideasconnections.microservicios.app.respuestas.models.entity.Respuesta;
 import com.ideasconnections.microservicios.app.respuestas.models.repository.RespuestaRepository;
 import com.ideasconnections.microservicios.commons.examenes.models.entity.Examen;
+import com.ideasconnections.microservicios.commons.examenes.models.entity.Pregunta;
 
 @Service
 public class RespuestaServiceImp implements RespuestaService {
@@ -28,7 +31,20 @@ public class RespuestaServiceImp implements RespuestaService {
 	@Override
 	@Transactional(readOnly = true)
 	public Iterable<Respuesta> findRespuestaByAlumnoByExamen(Long alumnoId, Long examenId) {
-		return null;
+		Examen examen = examenClient.obtenerExamenPorId(examenId);
+		List<Pregunta> preguntas = examen.getPreguntas();
+		List<Long> preguntaIds = preguntas.stream().map(pregunta -> pregunta.getId()).collect(Collectors.toList());
+		List<Respuesta> respuestas = (List<Respuesta>) repository.findRespuestaByAlumnoByPreguntaIds(alumnoId,
+				preguntaIds);
+		respuestas = respuestas.stream().map(respuesta -> {
+			preguntas.forEach(pregunta -> {
+				if (pregunta.getId() == respuesta.getPreguntaId()) {
+					respuesta.setPregunta(pregunta);
+				}
+			});
+			return respuesta;
+		}).collect(Collectors.toList());
+		return respuestas;
 	}
 
 	@Override
